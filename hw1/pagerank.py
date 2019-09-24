087f09c2d302c674760ef5508d9019b24fe40e4b
Dongkwan Kim <todoaskit@gmail.com>
2019-09-24 22:35:16 +0900

    Add hw1

--- hw1/pagerank.py
+++ hw1/pagerank.py
@@ -1,8 +1,63 @@
-import numpy as np
-from typing import Tuple, List
+from collections import defaultdict
+from typing import Tuple, List, Dict
+
+try:
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    import seaborn as sns
+except ModuleNotFoundError:
+    pass
 
 
-def pagerank(graph, damping_factor=0.85) -> List[Tuple[int, float]]:
+def tqdm_s(enumerable_instance):
+    try:
+        from tqdm import tqdm
+        return tqdm(enumerable_instance)
+    except ModuleNotFoundError:
+        return enumerable_instance
+
+
+def load_stanford_dataset(path="../web-Stanford.txt") -> List[Tuple[int, int]]:
+    print("Load SNAP...")
+    edges = []
+    with open(path, "r") as f:
+        for line in tqdm_s(f):
+            if not line.startswith("#"):
+                u, v = tuple([int(x) for x in line.strip().split()])
+                edges.append((u, v))
+    return edges
+
+
+def get_node_data_structure(graph: List[Tuple[int, int]]) \
+        -> Tuple[Dict[int, List[int]], Dict[int, List[int]], Dict[int, int]]:
+    node_idx_to_followers, node_idx_to_followings = defaultdict(list), defaultdict(list)
+
+    node_set = set()
+    for u, v in graph:
+        node_set.update([u, v])
+    node_id_to_index = {node_id: idx for idx, node_id in enumerate(node_set)}
+
+    # u --> v: u is follower of v, v is following of u.
+    for u, v in graph:
+        u_idx = node_id_to_index[u]
+        v_idx = node_id_to_index[v]
+        node_idx_to_followers[v_idx].append(u_idx)
+        node_idx_to_followings[u_idx].append(v_idx)
+
+    return node_idx_to_followers, node_idx_to_followings, node_id_to_index
+
+
+def get_degree(node_idx_to_followers, node_idx_to_followings, node_id_to_index) -> Tuple[List[int], List[int]]:
+    in_degree = [0 for _ in range(len(node_id_to_index))]
+    out_degree = [0 for _ in range(len(node_id_to_index))]
+    for node_idx, followers in node_idx_to_followers.items():
+        in_degree[node_idx] = len(followers)
+    for node_idx, followings in node_idx_to_followings.items():
+        out_degree[node_idx] = len(followings)
+    return in_degree, out_degree
+
+
+def pagerank(graph: List[Tuple[int, int]], damping_factor=0.85) -> List[Tuple[int, float]]:
     """
     Implement the variant of pagerank described in Section 2.2.
 
@@ -20,14 +75,134 @@ def pagerank(graph, damping_factor=0.85) -> List[Tuple[int, float]]:
         order of pagerank scores. If multiple nodes have the same pagerank
         score, then return them in any order.
     """
-    pass
+    print("\nPAGERANK with DF of {}".format(damping_factor))
+
+    node_idx_to_followers, node_idx_to_followings, node_id_to_index = get_node_data_structure(graph)
+    index_to_node_id = {index: node_id for node_id, index in node_id_to_index.items()}
+    in_degree, out_degree = get_degree(node_idx_to_followers, node_idx_to_followings, node_id_to_index)
+    in_degree_sum = sum(in_degree)
+
+    num_nodes = len(node_id_to_index)
+    rank_old = {node_idx: 1 / num_nodes for node_idx in range(num_nodes)}
+
+    diff, num_iter, eps = 100, 0, 0.0001
+    while diff > eps:
+        rank_new = {}
+
+        diff = 0
+        for node_idx in range(num_nodes):
+            followers_of_node = node_idx_to_followers[node_idx]
+            # Follows a link from the current page (with probability c).
+            if len(followers_of_node) > 0:
+                _rank_follows = sum(rank_old[f_idx] / out_degree[f_idx] for f_idx in followers_of_node)
+            # When the current web page does not have any links,
+            # the surfer always jumps to a random page.
+            else:
+                _rank_follows = 0
+
+            # Jumps to a random page (with prob. 1 - c)
+            # The random surfer chooses a destination considering the in-degree of web pages:
+            #   P(web page i is chosen) \prop 1 + d_{in}(i)
+            _rank_random = (1 + in_degree[node_idx]) / (num_nodes + in_degree_sum)
+
+            rank_new[node_idx] = damping_factor * _rank_follows + (1 - damping_factor) * _rank_random
+            diff += abs(rank_new[node_idx] - rank_old[node_idx])
+
+        rank_old = rank_new
+        num_iter += 1
+        print("ITER: {}, DIFF: {}".format(num_iter, diff))
+
+    return [(index_to_node_id[node_idx], rank) for node_idx, rank in rank_old.items()]
+
+
+def sorted_pagerank(graph: List[Tuple[int, int]], damping_factor=0.85):
+    pr = pagerank(graph, damping_factor)
+    return sorted(pr, key=lambda v: -v[1])
+
+
+def analyze_stanford_10(dataframe_path="../stanford_pagerank.pkl"):
+    try:
+        df = pd.read_pickle(dataframe_path)
+        print("Load at {}".format(dataframe_path))
+    except FileNotFoundError:
+        stanford_graph = load_stanford_dataset()
+
+        node_idx_to_followers, node_idx_to_followings, node_id_to_index = get_node_data_structure(stanford_graph)
+        in_degree, out_degree = get_degree(node_idx_to_followers, node_idx_to_followings, node_id_to_index)
+
+        damping_factor_list = [round(dfx10 * 0.1, 2) for dfx10 in range(1, 10, 2)]
+
+        rank_list = []
+        top10_node_set = set()
+        for damping_factor in damping_factor_list:
+            rank = sorted_pagerank(stanford_graph, damping_factor)
+            rank_list.append(rank)
+            top10_node_set.update([node_id for node_id, _ in rank[:10]])
+
+        data = []
+        for damping_factor, rank in zip(damping_factor_list, rank_list):
+            checked = set()
+            for i, (node_id, rank_value) in enumerate(rank):
+                if node_id in top10_node_set:
+                    checked.add(node_id)
+                    node_idx = node_id_to_index[node_id]
+                    data.append(
+                        [damping_factor, node_id, rank_value, i < 10, in_degree[node_idx], out_degree[node_idx]])
+
+                if len(checked) == len(top10_node_set):
+                    break
+        df = pd.DataFrame(
+            data=data,
+            columns=["damping factor", "node id", "pagerank", "highest 10 nodes", "in-degree", "out-degree"]
+        )
+        df.to_pickle(dataframe_path)
+        print("Save at {}".format(dataframe_path))
+
+    nodes_dec_by_in_degree = df.sort_values(["in-degree", "node id"], ascending=[False, False])
+    unique_nodes_dec_by_in_degree = pd.unique(nodes_dec_by_in_degree["node id"])
+
+    df_to_print = df.pivot(index="node id", columns="damping factor", values="pagerank")
+    print("\t".join(["node id \\ damping factor", "0.1", "0.3", "0.5", "0.7", "0.9"]))
+    for idx, r in df_to_print.iterrows():
+        print("\t".join([str(idx)] + [str(round(emt, 6)) for emt in r]))
+
+    sns.relplot(x="damping factor", y="pagerank", data=df,
+                legend="full", kind="line", col="node id", col_wrap=3, col_order=unique_nodes_dec_by_in_degree,
+                palette=sns.color_palette("tab20", len(pd.unique(df["node id"]))))
+    plt.savefig("./tex/figs/stanford_df_vs_pagerank.png", bbox_inches='tight')
+    plt.clf()
+
+    sns.relplot(x="in-degree", y="pagerank", hue="node id", data=df,
+                legend="full", col="damping factor", s=70, alpha=0.75,
+                palette=sns.color_palette("tab20", len(pd.unique(df["node id"]))))
+    plt.savefig("./tex/figs/stanford_indegree_vs_pagerank.png", bbox_inches='tight')
+    plt.clf()
+
+    sns.relplot(x="out-degree", y="pagerank", hue="node id", data=df,
+                legend="full", col="damping factor", s=70, alpha=0.75,
+                palette=sns.color_palette("tab20", len(pd.unique(df["node id"]))))
+    plt.savefig("./tex/figs/stanford_outdegree_vs_pagerank.png", bbox_inches='tight')
+    plt.clf()
 
 
 if __name__ == '__main__':
-    """
-    [(2, 0.425133489418432), (3, 0.37370009257188536), (5, 0.0759132065822711),
-     (4, 0.033845368431195004), (6, 0.033845368431195004), ... ]
-    """
-    pagerank([(2, 3), (3, 2), (4, 1), (4, 2), (5, 2), (5, 4),
-              (5, 6), (6, 2), (6, 5), (7, 2), (7, 5), (8, 2),
-              (8, 5), (9, 2), (9, 5), (10, 5), (11, 5)], 0.85)
+
+    MODE = "ANALYSIS"
+
+    if MODE == "SMALL":
+        """
+        [(2, 0.425133489418432), (3, 0.37370009257188536), (5, 0.0759132065822711),
+         (4, 0.033845368431195004), (6, 0.033845368431195004), ... ]
+        """
+        small_rank = sorted_pagerank([(2, 3), (3, 2), (4, 1), (4, 2), (5, 2), (5, 4),
+                                      (5, 6), (6, 2), (6, 5), (7, 2), (7, 5), (8, 2),
+                                      (8, 5), (9, 2), (9, 5), (10, 5), (11, 5)], 0.85)
+        print(small_rank[:10])
+
+    elif MODE == "STANFORD":
+        stanford_dataset = load_stanford_dataset()
+        stanford_rank = sorted_pagerank(stanford_dataset, 0.85)
+        print(stanford_rank[:10])
+
+    elif MODE == "ANALYSIS":
+        analyze_stanford_10()
--- hw1/random_graph.py
+++ hw1/random_graph.py
@@ -1,5 +1,112 @@
+from itertools import combinations
+from copy import deepcopy
+from pprint import pprint
+from typing import List, Tuple, Set
+from collections import Counter
 import numpy as np
-from typing import List, Tuple
+try:
+    import networkx as nx
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    import seaborn as sns
+except ModuleNotFoundError:
+    pass
+
+
+def tqdm_s(enumerable_instance):
+    try:
+        from tqdm import tqdm
+        return tqdm(enumerable_instance)
+    except ModuleNotFoundError:
+        return enumerable_instance
+
+
+def draw_networkx(edges_or_g):
+    if isinstance(edges_or_g, list):
+        G = nx.Graph()
+        G.add_edges_from(edges_or_g)
+    else:
+        G = edges_or_g
+    pos = nx.layout.spring_layout(G)
+    nx.draw_networkx_nodes(G, pos, node_size=5)
+    nx.draw_networkx_edges(G, pos, width=0.5, alpha=0.5)
+    ax = plt.gca()
+    ax.set_axis_off()
+    plt.show()
+
+
+def _attach_one_node(new_node: int, active_node_set: Set[int], u: float,
+                     degree_array: np.ndarray, attached_nodes: List[int]) -> Tuple[np.ndarray, list]:
+
+    # For each of the m edges of the new node, it is decided randomly (and independently) whether the
+    # edge connects to a random node, or to an active node.
+
+    # The former case happens with probability u, in which the random node is chosen based on
+    # preferential attachment (i.e., the chance of a node being chosen is proportional to its degree).
+    if np.random.random() < u:
+        if degree_array.sum() != 0:
+            possible_nodes = np.asarray([n for n in range(1, new_node) if n not in attached_nodes])
+            probs = degree_array[possible_nodes - 1]
+            probs /= probs.sum()
+            selected_node, = np.random.choice(possible_nodes, 1, p=probs)
+        else:  # Case of new_node = 2, active_node_set = {1}
+            selected_node = 1
+
+    # Of course, the latter case happens with probability 1 - u. In the latter case,
+    # the chance that each active node is chosen is the same. In both cases, the same node cannot be chosen as a
+    # destination multiple times. (i.e., there should be no duplicated edges).
+    else:
+        selected_node, = np.random.choice([n for n in active_node_set if n not in attached_nodes], 1)
+
+    # Update degree_array
+    degree_array[new_node - 1] += 1
+    degree_array[selected_node - 1] += 1
+
+    # Update attached_nodes
+    attached_nodes.append(selected_node)
+
+    return degree_array, attached_nodes
+
+
+def _preferential_attachment_with_intermediates(m, u, N, interval=None):
+
+    intermediates_edge_list = []
+
+    active_node_set = set(range(1, m + 1))
+    edge_list = list(combinations(range(1, m + 1), 2))
+    degree_array = np.zeros(N)
+    degree_array[:m] = m - 1
+
+    for new_node in range(m + 1, N + 1):
+
+        assert len(active_node_set) == m
+
+        # A new node arrives and forms m edges.
+        attached_nodes = []
+        for _ in range(m):
+            degree_array, attached_nodes = _attach_one_node(
+                new_node=new_node,
+                active_node_set=active_node_set,
+                u=u,
+                degree_array=degree_array,
+                attached_nodes=attached_nodes)
+        edge_list += [(selected_node, new_node) for selected_node in attached_nodes]
+
+        # An active node is chosen and turned inactive.
+        # The chance of a node being chosen is proportional to the inverse of its degree.
+        active_nodes = np.asarray(list(active_node_set))
+        inv_deg_of_active_nodes = (1 / degree_array[active_nodes - 1])
+        inactive_node, = np.random.choice(active_nodes, 1,
+                                          p=inv_deg_of_active_nodes / inv_deg_of_active_nodes.sum())
+        active_node_set.remove(inactive_node)
+
+        # The new node is turned active.
+        active_node_set.add(new_node)
+
+        if interval is not None and new_node % interval == 0:
+            intermediates_edge_list.append(deepcopy(edge_list))
+
+    return edge_list, intermediates_edge_list
 
 
 def preferential_attachment(m=8, u=0.1, N=10000) -> List[Tuple[int, int]]:
@@ -18,8 +125,94 @@ def preferential_attachment(m=8, u=0.1, N=10000) -> List[Tuple[int, int]]:
         then the output should be [(1,2), (2,3), (3,1)].
         The tuples do not have to be sorted in any order.
     """
-    pass
+    edge_list, _ = _preferential_attachment_with_intermediates(m, u, N, interval=None)
+    return edge_list
+
+
+def get_g_from_edges(edges):
+    g = nx.Graph()
+    g.add_edges_from(edges)
+    return g
+
+
+def analyze_effect_of_u(subjects=None, debug=False, seed=4):
+
+    np.random.seed(seed)
+    sns.set(style="ticks")
+
+    prefix = "large" if not debug else "small"
+    subjects = subjects or ["clustering_coefficient",
+                            "speed_of_increasing_of_average_path_length",
+                            "degree_distribution"]
+
+    u_list_11 = [round(0.1 * i, 1) for i in range(11)]
+
+    graph_kwargs = dict(m=8, N=10000, interval=2000) if not debug else dict(m=4, N=1000, interval=200)
+
+    g_list: List[nx.Graph] = []
+    itm_g_lists: List[List[nx.Graph]] = []
+    for i, u in enumerate(tqdm_s(u_list_11)):
+        edges, itm_edges_list = _preferential_attachment_with_intermediates(u=u, **graph_kwargs)
+        g = get_g_from_edges(edges)
+        g_list.append(g)
+
+        if i % 2 == 0:  # [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+            itm_g_lists.append([])
+            for interval_i, itm_edges in enumerate(itm_edges_list):
+                itm_g = get_g_from_edges(itm_edges)
+                itm_g_lists[-1].append(itm_g)
+
+    if "degree_distribution" in subjects:
+        print("\ndegree_distribution")
+        data = []
+        for i, u in enumerate(tqdm_s(u_list_11)):
+            if i % 2 == 0:
+                g = g_list[i]
+                degree_counter = Counter([d for n, d in g.degree])
+                for degree, frequency in degree_counter.items():
+                    data.append([u, np.log10(degree), np.log10(frequency)])
+        df = pd.DataFrame(data, columns=["mu", "log10 of degree", "log10 of frequency"])
+        sns.relplot(x="log10 of degree", y="log10 of frequency", col="mu", data=df, col_wrap=3)
+        plt.savefig("./tex/figs/{}_degree_distribution.png".format(prefix), bbox_inches='tight')
+        plt.clf()
+
+    if "clustering_coefficient" in subjects:
+        print("\nclustering_coefficient")
+        data = []
+        for i, u in enumerate(tqdm_s(u_list_11)):
+            g = g_list[i]
+            coefficient = nx.average_clustering(g)
+            data.append([u, coefficient])
+        df = pd.DataFrame(data, columns=["mu", "clustering coefficient"])
+        sns.relplot(x="mu", y="clustering coefficient", data=df, kind="line")
+        plt.savefig("./tex/figs/{}_clustering_coefficient.png".format(prefix), bbox_inches='tight')
+        plt.clf()
+
+    if "speed_of_increasing_of_average_path_length" in subjects:
+        print("\nspeed_of_increasing_of_average_path_length")
+        palette = sns.color_palette("Set1", 6)
+        u_to_color = {}
+        data = []
+        for i, itm_g_list in enumerate(tqdm_s(itm_g_lists)):
+            u = u_list_11[2 * i]
+            u_to_color[u] = palette.pop(0)
+            for interval_i, itm_g in enumerate(itm_g_list):
+                num_nodes = (interval_i + 1) * graph_kwargs["interval"]
+                average_path_length = nx.average_shortest_path_length(itm_g)
+                data.append([u, num_nodes, average_path_length])
+
+        df = pd.DataFrame(data, columns=["mu", "number of nodes", "average path length"])
+        sns.lmplot(x="number of nodes", y="average path length", hue="mu", data=df,
+                   legend="full", ci=None, scatter_kws={"s": 25, "alpha": 1}, palette=u_to_color)
+        plt.savefig("./tex/figs/{}_speed_of_inc_of_avg_path_length.png".format(prefix), bbox_inches='tight')
+        plt.clf()
+
+        df_mu_non_negative = df[df.mu > 0.0]
+        sns.lmplot(x="number of nodes", y="average path length", hue="mu", data=df_mu_non_negative,
+                   legend="full", ci=None, scatter_kws={"s": 25, "alpha": 1}, palette=u_to_color)
+        plt.savefig("./tex/figs/{}_speed_of_inc_of_avg_path_length_non_neg_mu.png".format(prefix), bbox_inches='tight')
+        plt.clf()
 
 
 if __name__ == '__main__':
-    edge_list = preferential_attachment()
+    analyze_effect_of_u(debug=False)
