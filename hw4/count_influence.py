4083aabee2bd0c44b99466f1852ab3684d3c0d32
Dongkwan Kim <todoaskit@gmail.com>
2019-11-14 01:36:30 +0900

    Add utils

--- hw4/count_influence.py
+++ hw4/count_influence.py
@@ -1,8 +1,70 @@
+from typing import Dict, List, Tuple
+
 import numpy as np
 # No external imports are allowed other than numpy
 
 
-def count_influence(graph, seeds, threshold):
+def tqdm_s(_iter):
+    try:
+        from tqdm import tqdm
+        return tqdm(_iter)
+    except ModuleNotFoundError:
+        return _iter
+
+
+def pbar_s(total):
+    try:
+        from tqdm import tqdm
+        return tqdm(total=total)
+    except ModuleNotFoundError:
+        return None
+
+
+def _remove_self_loops_and_parallel_edges(graph: List[Tuple[int, int]]):
+    graph = [(u, v) for u, v in set(graph) if u != v]
+    return graph
+
+
+def load_email_dataset(preprocess=True):
+    return load_dataset("../email-EuAll.txt", preprocess)
+
+
+def load_dataset(path, preprocess) -> List[Tuple[int, int]]:
+    _graph = []
+    with open(path, "r") as f:
+        for line in tqdm_s(f):
+            if not line.startswith("#"):
+                u, v = tuple(sorted([int(x) for x in line.strip().split()]))
+                _graph.append((u, v))
+    print("Loaded {}".format(path))
+    if preprocess:
+        _graph = _remove_self_loops_and_parallel_edges(_graph)
+    return _graph
+
+
+def _get_mapping(graph: List[Tuple[int, int]]):
+    node_set = set()
+    for u, v in graph:
+        node_set.update([u, v])
+    node_id_to_idx = {node_id: idx for idx, node_id in enumerate(node_set)}
+    idx_to_node_id = {idx: node_id for idx, node_id in enumerate(node_set)}
+    return node_set, node_id_to_idx, idx_to_node_id
+
+
+def _get_to_neighbors(graph: List[Tuple[int, int]], node_id_to_idx, N) -> Dict[int, List[int]]:
+    to_neighbors = {idx: [] for idx in range(N)}
+    for u, v in graph:
+        u, v = node_id_to_idx[u], node_id_to_idx[v]
+        to_neighbors[u].append(v)
+        to_neighbors[v].append(u)
+    return to_neighbors
+
+
+def _get_idx_to_degree(to_neighbors):
+    return {idx: len(to_neighbors[idx]) for idx in range(len(to_neighbors))}
+
+
+def count_influence(graph, seeds, threshold) -> int:
     """
     Implement the function that counts the ultimate number of active nodes.
 
@@ -17,3 +79,18 @@ def count_influence(graph, seeds, threshold):
 
     Output: the number of active nodes at time infinity.
     """
+    pass
+
+
+if __name__ == '__main__':
+
+    MODE = "email"
+
+    if MODE == "test":
+        pass
+
+    elif MODE == "email":
+        email_dataset = load_email_dataset()
+
+    else:
+        raise ValueError
--- hw4/seed_selection.py
+++ hw4/seed_selection.py
@@ -1,8 +1,70 @@
+from typing import Dict, List, Tuple
+
 import numpy as np
 # No external imports are allowed other than numpy
 
 
-def seed_selection(graph, policy, n):
+def tqdm_s(_iter):
+    try:
+        from tqdm import tqdm
+        return tqdm(_iter)
+    except ModuleNotFoundError:
+        return _iter
+
+
+def pbar_s(total):
+    try:
+        from tqdm import tqdm
+        return tqdm(total=total)
+    except ModuleNotFoundError:
+        return None
+
+
+def _remove_self_loops_and_parallel_edges(graph: List[Tuple[int, int]]):
+    graph = [(u, v) for u, v in set(graph) if u != v]
+    return graph
+
+
+def load_email_dataset(preprocess=True):
+    return load_dataset("../email-EuAll.txt", preprocess)
+
+
+def load_dataset(path, preprocess) -> List[Tuple[int, int]]:
+    _graph = []
+    with open(path, "r") as f:
+        for line in tqdm_s(f):
+            if not line.startswith("#"):
+                u, v = tuple(sorted([int(x) for x in line.strip().split()]))
+                _graph.append((u, v))
+    print("Loaded {}".format(path))
+    if preprocess:
+        _graph = _remove_self_loops_and_parallel_edges(_graph)
+    return _graph
+
+
+def _get_mapping(graph: List[Tuple[int, int]]):
+    node_set = set()
+    for u, v in graph:
+        node_set.update([u, v])
+    node_id_to_idx = {node_id: idx for idx, node_id in enumerate(node_set)}
+    idx_to_node_id = {idx: node_id for idx, node_id in enumerate(node_set)}
+    return node_set, node_id_to_idx, idx_to_node_id
+
+
+def _get_to_neighbors(graph: List[Tuple[int, int]], node_id_to_idx, N) -> Dict[int, List[int]]:
+    to_neighbors = {idx: [] for idx in range(N)}
+    for u, v in graph:
+        u, v = node_id_to_idx[u], node_id_to_idx[v]
+        to_neighbors[u].append(v)
+        to_neighbors[v].append(u)
+    return to_neighbors
+
+
+def _get_idx_to_degree(to_neighbors):
+    return {idx: len(to_neighbors[idx]) for idx in range(len(to_neighbors))}
+
+
+def seed_selection(graph: List[Tuple[int, int]], policy, n) -> List[int]:
     """
     Implement the function that chooses n initial active nodes.
 
@@ -20,3 +82,18 @@ def seed_selection(graph, policy, n):
 
     Outputs: a list of n integers, corresponding to n nodes.
     """
+    pass
+
+
+if __name__ == '__main__':
+
+    MODE = "email"
+
+    if MODE == "test":
+        pass
+
+    elif MODE == "email":
+        email_dataset = load_email_dataset()
+
+    else:
+        raise ValueError
