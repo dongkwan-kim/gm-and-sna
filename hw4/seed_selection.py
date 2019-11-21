05fcf4650942dd39104923c5ae2fb92ea4f8d2d0
Dongkwan Kim <todoaskit@gmail.com>
2019-11-22 00:26:48 +0900

    Add custom policy with pagerank

--- hw4/seed_selection.py
+++ hw4/seed_selection.py
@@ -68,6 +68,61 @@ def _get_idx_to_degree(to_neighbors):
     return {idx: len(to_neighbors[idx]) for idx in range(len(to_neighbors))}
 
 
+def pagerank(graph: List[Tuple[int, int]], damping_factor=0.85, to_sort=True) -> List[Tuple[int, float]]:
+    """
+    Implement the variant of pagerank described in Section 2.2.
+
+    Inputs:
+        graph: undirected input graph in the edge list format.
+
+    Output: a list of tuples of the form (v, score), which indicates node v
+        and its pagerank score. The list should be sorted in the decreasing
+        order of pagerank scores. If multiple nodes have the same pagerank
+        score, then return them in any order.
+    """
+    print("\nPAGERANK with DF of {}".format(damping_factor))
+
+    node_set, node_id_to_idx, idx_to_node_id = _get_mapping(graph)
+    num_nodes = len(node_set)
+    to_neighbors = _get_to_neighbors(graph, node_id_to_idx, num_nodes)
+    degree = [len(to_neighbors[node_idx]) for node_idx in range(num_nodes)]
+
+    rank_old = {node_idx: 1 / num_nodes for node_idx in range(num_nodes)}
+
+    diff, num_iter, eps = 100, 0, 0.0001
+    while diff > eps:
+        rank_new = {}
+
+        diff = 0
+        for node_idx in range(num_nodes):
+            neighbors = to_neighbors[node_idx]
+            # Follows a link from the current page (with probability c).
+            if len(neighbors) > 0:
+                _rank_neighbors = sum(rank_old[n_idx] / degree[n_idx] for n_idx in neighbors)
+            # When the current web page does not have any links,
+            # the surfer always jumps to a random page.
+            else:
+                _rank_neighbors = 0
+
+            # Jumps to a random page (with prob. 1 - c)
+            # The random surfer chooses a destination considering the degree of web pages:
+            #   P(web page i is chosen) \prop 1 + d(i)
+            _rank_random = (1 + degree[node_idx]) / (num_nodes + len(degree))
+
+            rank_new[node_idx] = damping_factor * _rank_neighbors + (1 - damping_factor) * _rank_random
+            diff += abs(rank_new[node_idx] - rank_old[node_idx])
+
+        rank_old = rank_new
+        num_iter += 1
+        print("ITER: {}, DIFF: {}".format(num_iter, diff))
+
+    nid_and_rank = [(idx_to_node_id[node_idx], rank) for node_idx, rank in rank_old.items()]
+    if to_sort:
+        return sorted(nid_and_rank, key=lambda emt: -emt[1])
+    else:
+        return nid_and_rank
+
+
 def seed_selection(graph: List[Tuple[int, int]], policy, n) -> List[int]:
     """
     Implement the function that chooses n initial active nodes.
@@ -100,7 +155,9 @@ def seed_selection(graph: List[Tuple[int, int]], policy, n) -> List[int]:
         return list(selected_nodes)
 
     elif policy == "custom":
-        raise NotImplementedError
+        node_id_sorted_by_pr = [node_id for node_id, _ in pagerank(graph)]
+        selected_node_ids = node_id_sorted_by_pr[:n]
+        return selected_node_ids
 
     else:
         raise ValueError
@@ -108,19 +165,28 @@ def seed_selection(graph: List[Tuple[int, int]], policy, n) -> List[int]:
 
 if __name__ == '__main__':
 
-    MODE = "test"
+    MODE = "custom"
 
     if MODE == "test":
         G = nx.karate_club_graph()
 
-        random_selected = seed_selection(graph=G.edges, policy="random", n=2)
+        random_selected = seed_selection(graph=G.edges, policy="random", n=10)
+        degree_selected = seed_selection(graph=G.edges, policy="degree", n=10)
+        custom_selected = seed_selection(graph=G.edges, policy="custom", n=10)
+
         print(random_selected)
-
-        degree_selected = seed_selection(graph=G.edges, policy="degree", n=2)
         print(degree_selected)
+        print(custom_selected)
 
-    elif MODE == "email":
+    elif MODE == "pagerank":
         email_dataset = load_email_dataset()
+        pr = pagerank(email_dataset)
+        print(pr[:10])
+
+    elif MODE == "custom":
+        email_dataset = load_email_dataset()
+        custom_selected = seed_selection(email_dataset, "custom", 10)
+        print(custom_selected)
 
     else:
         raise ValueError
